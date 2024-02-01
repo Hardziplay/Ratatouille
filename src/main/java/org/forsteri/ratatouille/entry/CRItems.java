16a6780908e7d1f4b33b78eae393f58253b7df6a
Neve133 <hzh15914420771@gmail.com>
2024-02-01 22:38:36 +0800

    update:item reg

--- src/main/java/org/forsteri/ratatouille/entry/CRItems.java
+++ src/main/java/org/forsteri/ratatouille/entry/CRItems.java
@@ -15,6 +15,16 @@ public class CRItems {
     public static final ItemEntry<Item> SAUSAGE = Ratatouille.REGISTRATE.item("sausage", Item::new).register();
     public static final ItemEntry<Item> RAW_SAUSAGE = Ratatouille.REGISTRATE.item("raw_sausage", Item::new).register();
     public static final ItemEntry<Item> SALT = Ratatouille.REGISTRATE.item("salt", Item::new).register();
+    public static final ItemEntry<Item> CAKE_MOLD = Ratatouille.REGISTRATE.item("cake_mold", Item::new).register();
+    public static final ItemEntry<Item> CHOCOLATE_MOLD = Ratatouille.REGISTRATE.item("chocolate_mold", Item::new).register();
+    public static final ItemEntry<Item> COCOABEAN_POWDER = Ratatouille.REGISTRATE.item("cocoabean_powder", Item::new).register();
+    public static final ItemEntry<Item> COCOABUTTER = Ratatouille.REGISTRATE.item("cocoabutter", Item::new).register();
+    public static final ItemEntry<Item> DRIED_COCOABEANS = Ratatouille.REGISTRATE.item("dried_cocoabeans", Item::new).register();
+    public static final ItemEntry<Item> DRIED_COCOANIBS = Ratatouille.REGISTRATE.item("dried_cocoanibs", Item::new).register();
+    public static final ItemEntry<Item> ICE_CRYSTAL = Ratatouille.REGISTRATE.item("ice_crystal", Item::new).register();
+    public static final ItemEntry<Item> VANILLA_POWDER = Ratatouille.REGISTRATE.item("vanilla_powder", Item::new).register();
+    public static final ItemEntry<Item> SUGAR_BAR = Ratatouille.REGISTRATE.item("sugar_bar", Item::new).register();
+
     public CRItems() {}
 
     public static void register() {}
