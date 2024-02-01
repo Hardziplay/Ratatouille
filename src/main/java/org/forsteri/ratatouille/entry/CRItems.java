b41f12e9af2afc96760c08a79ffcb375650488f3
Neve133 <hzh15914420771@gmail.com>
2024-02-01 22:01:01 +0800

    update:item reg

--- src/main/java/org/forsteri/ratatouille/entry/CRItems.java
+++ src/main/java/org/forsteri/ratatouille/entry/CRItems.java
@@ -12,7 +12,9 @@ public class CRItems {
     }
 
     public static final ItemEntry<Item> SAUSAGE_CASING = Ratatouille.REGISTRATE.item("sausage_casing", Item::new).register();
-
+    public static final ItemEntry<Item> SAUSAGE = Ratatouille.REGISTRATE.item("sausage", Item::new).register();
+    public static final ItemEntry<Item> RAW_SAUSAGE = Ratatouille.REGISTRATE.item("raw_sausage", Item::new).register();
+    public static final ItemEntry<Item> SALT = Ratatouille.REGISTRATE.item("salt", Item::new).register();
     public CRItems() {}
 
     public static void register() {}
