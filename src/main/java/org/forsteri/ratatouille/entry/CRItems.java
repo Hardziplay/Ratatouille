f4c2b6b7cc22cec97ffa8a1568f8215655c2ae51
Neve133 <hzh15914420771@gmail.com>
2024-02-04 00:12:20 +0800

    update:cocoa

--- src/main/java/org/forsteri/ratatouille/entry/CRItems.java
+++ src/main/java/org/forsteri/ratatouille/entry/CRItems.java
@@ -21,6 +21,7 @@ public class CRItems {
     public static final ItemEntry<Item> COCOA_BUTTER = Ratatouille.REGISTRATE.item("cocoa_butter", Item::new).register();
     public static final ItemEntry<Item> DRIED_COCOA_BEANS = Ratatouille.REGISTRATE.item("dried_cocoa_beans", Item::new).register();
     public static final ItemEntry<Item> DRIED_COCOA_NIBS = Ratatouille.REGISTRATE.item("dried_cocoa_nibs", Item::new).register();
+    public static final ItemEntry<Item> COCOA_BAR = Ratatouille.REGISTRATE.item("cocoa_bar", Item::new).register();
     public static final ItemEntry<Item> ICE_CRYSTAL = Ratatouille.REGISTRATE.item("ice_crystal", Item::new).register();
     public static final ItemEntry<Item> VANILLA_POWDER = Ratatouille.REGISTRATE.item("vanilla_powder", Item::new).register();
     public static final ItemEntry<Item> SUGAR_BAR = Ratatouille.REGISTRATE.item("sugar_bar", Item::new).register();
