0fec8a1bf46f0b85f6b197ea21e69c035f97cb9e
thaumstrial <2738722282@qq.com>
2024-02-25 10:20:38 +0800

    feat: mechanical demolder block

--- src/main/java/org/forsteri/ratatouille/content/demoulder/MechanicalDemolderBlock.java
+++ src/main/java/org/forsteri/ratatouille/content/demoulder/MechanicalDemolderBlock.java
@@ -1,22 +1,32 @@
 package org.forsteri.ratatouille.content.demoulder;
 
+import com.simibubi.create.AllShapes;
 import com.simibubi.create.content.kinetics.base.HorizontalKineticBlock;
 import com.simibubi.create.content.kinetics.press.PressingBehaviour;
 import com.simibubi.create.foundation.block.IBE;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.forsteri.ratatouille.entry.CRBlockEntityTypes;
 
 public class MechanicalDemolderBlock  extends HorizontalKineticBlock implements IBE<MechanicalDemolderBlockEntity> {
 
     public MechanicalDemolderBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return AllShapes.CASING_12PX.get(Direction.DOWN);
     }
 
     @Override
