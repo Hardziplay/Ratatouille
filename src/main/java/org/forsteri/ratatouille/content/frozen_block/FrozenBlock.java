ea76ecfc3d6a4f3694e18050026a7d22f634d6d7
thaumstrial <2738722282@qq.com>
2024-03-03 12:40:02 +0800

    feat: frozen block

---
+++ src/main/java/org/forsteri/ratatouille/content/frozen_block/FrozenBlock.java
@@ -0,0 +1,26 @@
+package org.forsteri.ratatouille.content.frozen_block;
+
+import com.simibubi.create.foundation.utility.VecHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+
+public class FrozenBlock extends Block {
+    public FrozenBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public void animateTick(BlockState pState, Level pLevel, BlockPos pPos, RandomSource pRand) {
+        if (pRand.nextInt(5) != 0)
+            return;
+        Vec3 vec3 = VecHelper.clampComponentWise(VecHelper.offsetRandomly(Vec3.ZERO, pRand, .75f), .55f)
+                .add(VecHelper.getCenterOf(pPos));
+        pLevel.addParticle(ParticleTypes.END_ROD, vec3.x, vec3.y, vec3.z, pRand.nextGaussian() * 0.005D,
+                pRand.nextGaussian() * 0.005D, pRand.nextGaussian() * 0.005D);
+    }
+}
