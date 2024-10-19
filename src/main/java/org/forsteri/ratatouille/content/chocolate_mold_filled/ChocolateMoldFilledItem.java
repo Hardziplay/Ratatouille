494db733840ec86baec5aec85e3bcb3f87395d20
thaumstrial <2738722282@qq.com>
2024-10-19 22:22:25 +0800

    feat: custom recipe for frozen block

--- src/main/java/org/forsteri/ratatouille/content/chocolate_mold_filled/ChocolateMoldFilledItem.java
+++ src/main/java/org/forsteri/ratatouille/content/chocolate_mold_filled/ChocolateMoldFilledItem.java
@@ -15,58 +15,29 @@ import org.jetbrains.annotations.NotNull;
 
 public class ChocolateMoldFilledItem extends Item {
 
-    public static int MAX_CHILLNESS = 50;
+//    public static int MAX_CHILLNESS = 50;
     public ChocolateMoldFilledItem(Properties pProperties) {
         super(pProperties);
     }
-
-    public int getChillness(ItemStack stack) {
-        return stack.getOrCreateTag()
-                .getInt("CollectingChillness");
-    }
-    @Override
-    public boolean isBarVisible(ItemStack stack) {
-        return getChillness(stack) > 0;
-    }
-
-
-    @Override
-    public int getBarWidth(ItemStack stack) {
-        return Math.round(13.0F * getChillness(stack) / MAX_CHILLNESS);
-    }
-
-    @Override
-    public int getBarColor(ItemStack stack) {
-        return Color.mixColors(0x413c69, 0xFFFFFF,
-                getChillness(stack) / (float) MAX_CHILLNESS);
-    }
-
-    @Override
-    public boolean onEntityItemUpdate(ItemStack stack, @NotNull ItemEntity entity) {
-        Level level = entity.getLevel();
-        CompoundTag itemData = entity.getItem()
-                .getOrCreateTag();
-        if (itemData.getInt("CollectingChillness") >= MAX_CHILLNESS) {
-            ItemStack newStack = CRItems.CHOCOLATE_MOLD_SOLID.asStack();
-            ItemEntity newEntity = new ItemEntity(level, entity.getX(), entity.getY(), entity.getZ(), newStack);
-            newEntity.setDeltaMovement(entity.getDeltaMovement());
-            itemData.remove("CollectingChillness");
-            level.addFreshEntity(newEntity);
-
-            stack.split(1);
-            entity.setItem(stack);
-            if (stack.isEmpty())
-                entity.discard();
-            return false;
-        }
-        BlockPos entityPos = entity.getOnPos();
-        for (BlockPos blockPos : BlockPos.betweenClosed(entityPos.offset(-1, -1, -1), entityPos.offset(1, 1, 1))) {
-            if (level.getBlockState(blockPos).is(CRBlocks.FROZEN_BLOCK.get())) {
-                stack.getOrCreateTag()
-                        .putInt("CollectingChillness", itemData.getInt("CollectingChillness") + 1);
-            }
-        }
-        return false;
-    }
-
+//
+//    public int getChillness(ItemStack stack) {
+//        return stack.getOrCreateTag()
+//                .getInt("CollectingChillness");
+//    }
+//    @Override
+//    public boolean isBarVisible(ItemStack stack) {
+//        return getChillness(stack) > 0;
+//    }
+//
+//
+//    @Override
+//    public int getBarWidth(ItemStack stack) {
+//        return Math.round(13.0F * getChillness(stack) / MAX_CHILLNESS);
+//    }
+//
+//    @Override
+//    public int getBarColor(ItemStack stack) {
+//        return Color.mixColors(0x413c69, 0xFFFFFF,
+//                getChillness(stack) / (float) MAX_CHILLNESS);
+//    }
 }
