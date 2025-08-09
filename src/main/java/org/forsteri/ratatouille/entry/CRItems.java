6657a653b637666c1505b64b5faad861f387363e
thaumstrial <2738722282@qq.com>
2025-08-09 21:40:51 +0800

    update: sequenced assembly item

--- src/main/java/org/forsteri/ratatouille/entry/CRItems.java
+++ src/main/java/org/forsteri/ratatouille/entry/CRItems.java
@@ -1,5 +1,6 @@
 package org.forsteri.ratatouille.entry;
 
+import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.ItemEntry;
 import net.minecraft.world.food.FoodProperties;
@@ -49,9 +50,9 @@ public class CRItems {
             .register();
     public static final ItemEntry<Item> COMPOST_RESIDUE = Ratatouille.REGISTRATE.item("compost_residue", Item::new).register();
     public static final ItemEntry<Item> RIPEN_MATTER = Ratatouille.REGISTRATE.item("ripen_matter", Item::new).register();
-    public static final ItemEntry<Item> UNPROCESSED_RIPEN_MATTER = Ratatouille.REGISTRATE.item("unprocessed_ripen_matter", Item::new).register();
+    public static final ItemEntry<SequencedAssemblyItem> UNPROCESSED_RIPEN_MATTER = Ratatouille.REGISTRATE.item("unprocessed_ripen_matter", SequencedAssemblyItem::new).register();
     public static final ItemEntry<Item> MATURE_MATTER = Ratatouille.REGISTRATE.item("mature_matter", Item::new).register();
-    public static final ItemEntry<Item> UNPROCESSED_MATURE_MATTER = Ratatouille.REGISTRATE.item("unprocessed_mature_matter", Item::new).register();
+    public static final ItemEntry<SequencedAssemblyItem> UNPROCESSED_MATURE_MATTER = Ratatouille.REGISTRATE.item("unprocessed_mature_matter", SequencedAssemblyItem::new).register();
     public static final ItemEntry<Item> BOIL_STONE = Ratatouille.REGISTRATE.item("boil_stone", Item::new).register();
     public static final ItemEntry<Item> COMPOST_MASS = Ratatouille.REGISTRATE.item("compost_mass", Item::new).register();
     //public static final ItemEntry<Item> WET_COPPER_INGOT = Ratatouille.REGISTRATE.item("wet_copper_ingot", Item::new).register();
