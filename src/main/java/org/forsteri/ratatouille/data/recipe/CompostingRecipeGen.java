19d2015bb4dc65bca0f3795b9b1bb31984f8fa93
Neve133 <hzh15914420771@gmail.com>
2025-08-12 00:05:07 +0900

    fix: compost recipes

--- src/main/java/org/forsteri/ratatouille/data/recipe/CompostingRecipeGen.java
+++ src/main/java/org/forsteri/ratatouille/data/recipe/CompostingRecipeGen.java
@@ -11,8 +11,8 @@ public class CompostingRecipeGen extends ProcessingRecipeGen {
             .require(CRItems.COMPOST_MASS.get())
             .duration(200)
             //.output(CRItems.COMPOST_RESIDUE.get(), 1)
-            .output(CRFluids.COMPOST_RESIDUE_FLUID.get(), 20)
-            .output(CRFluids.COMPOST_TEA.get(), 60)
+            .output(CRFluids.COMPOST_RESIDUE_FLUID.get(), 60)
+            .output(CRFluids.COMPOST_TEA.get(), 20)
             .output(CRFluids.BIO_GAS.get(), 20));
 
     public CompostingRecipeGen(PackOutput generator) {
