1060192f40d8819f598f745d2b5ee103dbdabe3d
thaumstrial <2738722282@qq.com>
2025-08-24 11:45:51 +0800

    update: farmer's delight dependency #14

--- src/main/java/org/forsteri/ratatouille/mixin/RichSoilFarmlandBlockMixin.java
+++ src/main/java/org/forsteri/ratatouille/mixin/RichSoilFarmlandBlockMixin.java
@@ -3,7 +3,6 @@ package org.forsteri.ratatouille.mixin;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.FarmBlock;
-import net.minecraftforge.fluids.capability.IFluidHandler;
 import org.forsteri.ratatouille.content.irrigation_tower.IrrigationTowerBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,12 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vectorwing.farmersdelight.common.block.RichSoilFarmlandBlock;
 
 @Mixin(value = RichSoilFarmlandBlock.class, remap = false)
-public class RichSoilFarmlandBlockMixin extends FarmBlock{
+public class RichSoilFarmlandBlockMixin extends FarmBlock {
     public RichSoilFarmlandBlockMixin(Properties pProperties) {
         super(pProperties);
     }
 
-    @Inject(method = "IsNearWater", at=@At("HEAD"), cancellable = true)
+    @Inject(method = "isNearWater", at = @At("HEAD"), cancellable = true)
     private static void hasWater(LevelReader pLevel, BlockPos pPos, CallbackInfoReturnable<Boolean> cir) {
         IrrigationTowerBlockEntity.isNearWater(pLevel, pPos, cir);
     }
